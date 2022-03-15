c6d780f943ff2ae8e64069dcbe9ba2fb4745c1c1
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-03-15 17:33:12 +0100

    Created Player

---
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -0,0 +1,70 @@
+package it.polimi.ingsw.model.player;
+
+import it.polimi.ingsw.model.enumerations.Card;
+import it.polimi.ingsw.model.enumerations.CardBack;
+import it.polimi.ingsw.model.enumerations.TowerColor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    int ID;
+    String name;
+    School school;
+    int numCoins = 0;
+    List<Card> playableCards = new ArrayList<>();
+    Card selectedCard = null;
+    TowerColor teamColor;
+    CardBack cardBack;
+
+    public Player(int ID, String name, TowerColor teamColor, CardBack cardBack) {
+        this.ID = ID;
+        this.name = name;
+        this.teamColor = teamColor;
+        this.cardBack = cardBack;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public int getNumCoins() {
+        return numCoins;
+    }
+
+    public void addCoins(int amount) {
+        numCoins += amount;
+    }
+
+    public void removeCoins(int amount) {
+        numCoins -= amount;
+    }
+
+    public List<Card> getPlayableCards() {
+        return new ArrayList<>(playableCards);
+    }
+
+    public Card getSelectedCard() {
+        return selectedCard;
+    }
+
+    public void setSelectedCard(Card selectedCard) {
+        this.selectedCard = selectedCard;
+    }
+
+    public TowerColor getTeamColor() {
+        return teamColor;
+    }
+
+    public CardBack getCardBack() {
+        return cardBack;
+    }
+}
