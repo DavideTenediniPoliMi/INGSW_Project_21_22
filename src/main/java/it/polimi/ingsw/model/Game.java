bc68e7ac3d2e7f3179b98bcb883c78f905beb829
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-03-15 21:26:13 +0100

    Created Game

---
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private Board board;
+    private final List<Player> players = new ArrayList<>();
+    private int numPlayersStillToAct;
+
+    public Game(int numPlayers, boolean expertModeActive) {
+        this.numPlayersStillToAct = numPlayers;
+        // TODO
+    }
+
+    // TODO
+    public void startGame() {}
+
+    public void nextRound() {}
+
+    public void endGame() {}
+
+    private void decidePlayingOrder() {}
+}
