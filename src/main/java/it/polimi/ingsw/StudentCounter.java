eefb0222896ec8b1066a6a4b44a9785703341d7a
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-03-15 13:02:37 +0100

    Created StudentCounter

---
+++ src/main/java/it/polimi/ingsw/StudentCounter.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw;
+
+public class StudentCounter {
+    private int[] counters;
+
+    public StudentCounter() {
+        this.counters = new int[5]; // already initialized to 0
+    }
+
+    public int getByColor(Color c) {
+        return counters[c.ordinal()];
+    }
+
+    public void addByColor(Color c, int numStudents) {
+        counters[c.ordinal()] += numStudents;
+    }
+
+    public void transferByColorTo(StudentCounter recipient, Color c, int numStudents) {
+        // TODO
+    }
+
+    public void transferAllTo(StudentCounter recipient) {
+        // TODO
+    }
+}
