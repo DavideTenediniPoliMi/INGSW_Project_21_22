e2bc8de8af2855ba87d76c809c929ec89303dc5f
sprocatitommaso <tommaso.sprocati@mail.polimi.it>
2022-03-23 20:06:17 +0100

    Updated cards ExchangeStudentsDecorator and StudentGroupDecorator

--- src/main/java/it/polimi/ingsw/model/characters/ExchangeStudentsDecorator.java
+++ src/main/java/it/polimi/ingsw/model/characters/ExchangeStudentsDecorator.java
@@ -2,25 +2,37 @@ package it.polimi.ingsw.model.characters;
 
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.helpers.Parameters;
 import it.polimi.ingsw.model.helpers.StudentGroup;
 
 public class ExchangeStudentsDecorator extends CharacterCardDecorator{
+    private int playerID;
+    private final StudentGroup fromEntrance;
+    private final StudentGroup fromDiningRoom;
 
     public ExchangeStudentsDecorator(CharacterCard card){
         super(card);
+        this.fromEntrance = new StudentGroup();
+        this.fromDiningRoom = new StudentGroup();
     }
 
     public int activate(){
         card.activate();
-        return 0;
-    }
-
-    public void swap(int playerID, StudentGroup fromEntrance, StudentGroup fromDiningRoom){
         Board board = Game.getInstance().getBoard();
 
         board.removeFromEntranceOf(playerID, fromEntrance);
         board.removeFromDiningRoomOf(playerID, fromDiningRoom);
+
         board.addToEntranceOf(playerID, fromDiningRoom);
         board.addToDiningRoomOf(playerID, fromEntrance);
+
+        return -1;
+    }
+
+    public void setParameters(Parameters params){
+        this.playerID = params.getPlayerIndex();
+
+        params.getFromOrigin().transferAllTo(fromEntrance);
+        params.getFromDestination().transferAllTo(fromDiningRoom);
     }
 }
--- src/main/java/it/polimi/ingsw/model/characters/StudentGroupDecorator.java
+++ src/main/java/it/polimi/ingsw/model/characters/StudentGroupDecorator.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.model.characters;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Island;
 import it.polimi.ingsw.model.board.School;
+import it.polimi.ingsw.model.helpers.Parameters;
 import it.polimi.ingsw.model.helpers.StudentBag;
 import it.polimi.ingsw.model.helpers.StudentGroup;
 
@@ -11,15 +12,18 @@ public class StudentGroupDecorator extends CharacterCardDecorator{
     private final int TRANSFER_CARD_STUDENTS = 4;
 
     private final StudentGroup students;
-    private final boolean toIslands, toDiningRoom;
+    private final boolean isToIsland, isToDiningRoom;
+    private final StudentGroup fromCard = new StudentGroup();
+    private final StudentGroup fromEntrance = new StudentGroup();
+    private int islandIndex, playerIndex;
 
-    public StudentGroupDecorator(GenericCard card, boolean toIslands, boolean toDiningRoom){
+    public StudentGroupDecorator(GenericCard card, boolean isToIsland, boolean isToDiningRoom){
         super(card);
 
-        this.toIslands = toIslands;
-        this.toDiningRoom = toDiningRoom;
+        this.isToIsland = isToIsland;
+        this.isToDiningRoom = isToDiningRoom;
 
-        if(!toIslands && !toDiningRoom){
+        if(!isToIsland && !isToDiningRoom){
             students = StudentBag.getBag().drawStudents(SWAP_CARD_STUDENTS);
         }else{
             students = StudentBag.getBag().drawStudents(TRANSFER_CARD_STUDENTS);
@@ -28,43 +32,41 @@ public class StudentGroupDecorator extends CharacterCardDecorator{
 
     public int activate(){
         card.activate();
-        return 0;
-    }
+        StudentGroup temp = new StudentGroup();
 
-    public void transferTo(Object destination, StudentGroup[] selectedStudents){
-        if(card.isActive()){
-            StudentGroup tmp = new StudentGroup();
-            if(toIslands && destination instanceof Island){
-                Island i = (Island) destination;
-                students.transferTo(tmp, selectedStudents[0]);
-                Game.getInstance().getBoard().addStudentsToIsland(tmp, i);
+        students.transferTo(temp, fromCard);
 
-                refillStudents();
-            }
-            else if(destination instanceof School){
-                School s = (School) destination;
-                int playerID = s.getOwner().getID();
-                if(toDiningRoom){
-                    students.transferTo(tmp, selectedStudents[0]);
+        if(isToIsland){
+            Game.getInstance().getBoard().addStudentsToIsland(fromCard, islandIndex);
 
-                    Game.getInstance().getBoard().addToDiningRoomOf(playerID, tmp);
+            refillStudents();
+        }else if(isToDiningRoom){
+            Game.getInstance().getBoard().addToDiningRoomOf(playerIndex, fromCard);
 
-                    refillStudents();
-                }else{
-                    if(selectedStudents.length > 1){
-                        students.transferTo(tmp, selectedStudents[0]);
+            refillStudents();
+        }else{
+            Game.getInstance().getBoard().removeFromEntranceOf(playerIndex, fromEntrance);
+            Game.getInstance().getBoard().addToEntranceOf(playerIndex, temp);
 
-                        Game.getInstance().getBoard().removeFromEntranceOf(playerID, selectedStudents[1]);
-                        Game.getInstance().getBoard().addToEntranceOf(playerID, tmp);
-
-                        selectedStudents[1].transferAllTo(students);
-                    }
-                }
-            }
+            fromEntrance.transferAllTo(students);
         }
+
+        return -1;
     }
 
     private void refillStudents(){
         StudentBag.getBag().drawStudents(1).transferAllTo(students);
+    }
+
+    public void setParameters(Parameters params){
+        params.getFromOrigin().transferAllTo(fromCard);
+        if(isToIsland) {
+            this.islandIndex = params.getIslandIndex();
+        }else if(isToDiningRoom){
+            this.playerIndex = params.getPlayerIndex();
+        }else{
+            params.getFromDestination().transferAllTo(fromEntrance);
+            this.playerIndex = params.getPlayerIndex();
+        }
     }
 }
