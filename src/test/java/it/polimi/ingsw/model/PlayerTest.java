4f3c7b443dc5f4dd03affe355bd353581dd0ef76
sprocatitommaso <tommaso.sprocati@mail.polimi.it>
2022-03-29 23:05:07 +0200

    Added Player test no complete coverage

--- src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.enumerations.Card;
+import it.polimi.ingsw.model.enumerations.CardBack;
+import it.polimi.ingsw.model.enumerations.TowerColor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,52 +10,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    Player player;
 
     @BeforeEach
     void setUp() {
+        player = new Player(0, "mario", TowerColor.BLACK, CardBack.CB_1, true);
     }
 
     @AfterEach
     void tearDown() {
-    }
-
-    @Test
-    void getID() {
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void getTeamColor() {
-    }
-
-    @Test
-    void getCardBack() {
-    }
-
-    @Test
-    void isTowerHolder() {
-    }
-
-    @Test
-    void getSelectedCard() {
+        player = null;
     }
 
     @Test
     void setSelectedCard() {
-    }
+        Card c = Card.CARD_1;
 
-    @Test
-    void getNumCoins() {
+        assertNull(player.getSelectedCard());
+        player.setSelectedCard(c);
+        assertEquals(Card.CARD_1, player.getSelectedCard());
     }
 
     @Test
     void addCoin() {
+        int coinsBefore = player.getNumCoins();
+        player.addCoin();
+        assertEquals(coinsBefore + 1, player.getNumCoins());
     }
 
     @Test
     void removeCoins() {
+        int coinsBefore = player.getNumCoins();
+        int amount = 2;
+
+        player.removeCoins(2);
+        assertEquals(coinsBefore - amount, player.getNumCoins());
     }
 }
