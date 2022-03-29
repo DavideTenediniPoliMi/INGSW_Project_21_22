f6b8f764e2a6d25b8db01f8d6e10be80acd9fb53
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-03-29 23:06:36 +0200

    Added some tests for Game

--- src/test/java/it/polimi/ingsw/model/GameTest.java
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -1,20 +1,41 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.enumerations.CardBack;
+import it.polimi.ingsw.model.enumerations.TowerColor;
 import junit.framework.TestCase;
 
 public class GameTest extends TestCase {
 
-    public void setUp() {
+    Game g;
 
+    public void setUp() {
+        g = Game.getInstance();
+
+        g.addPlayer(0, "ezio", TowerColor.WHITE, CardBack.CB_1, true);
+        g.addPlayer(1, "bruso", TowerColor.GREY, CardBack.CB_2, true);
+
+        g.addTowersTo(0, 8);
+        g.addTowersTo(1, 8);
+
+        g.createClouds(3);
     }
 
     public void tearDown() {
+        Game.resetInstance();
+        g = null;
     }
 
     public void testConquerIsland() {
+        g.conquerIsland(0, 0);
+        assertEquals(g.getPlayerByID(0).getTeamColor(), g.getBoard().getIslandAt(0).getTeamColor());
     }
 
     public void testMergeIslands() {
+        g.conquerIsland(0, 0);
+        g.conquerIsland(1, 0);
+
+        g.mergeIslands(0, 1);
+        assertEquals(g.getBoard().getIslandAt(0).getNumIslands(), 2);
     }
 
     public void testPlaceMNAt() {
