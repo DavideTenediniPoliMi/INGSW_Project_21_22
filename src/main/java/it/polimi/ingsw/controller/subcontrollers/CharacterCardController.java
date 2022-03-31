56f21e6a1b6f97e81ea57610de26acb85343375c
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-03-31 15:15:41 +0200

    Added subcontroller package + CharacterCardController

---
+++ src/main/java/it/polimi/ingsw/controller/subcontrollers/CharacterCardController.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.controller.subcontrollers;
+
+import it.polimi.ingsw.model.enumerations.EffectType;
+import it.polimi.ingsw.model.helpers.Parameters;
+
+public class CharacterCardController {
+    private boolean effectUsed;
+
+    public CharacterCardController() {
+
+    }
+
+    public void buyCharacterCard(int playerID, int cardIndex) {
+
+    }
+
+    public void setCardParameters(Parameters params) {
+
+    }
+
+    public boolean isActiveCardOfType(EffectType effectType) {
+        return false;
+    }
+
+    public int activateCard() {
+        return 0;
+    }
+
+    private boolean checkParameters(Parameters params) {
+        return false;
+    }
+}
