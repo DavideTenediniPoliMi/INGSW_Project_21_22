cf2a86f155a75a7cb75266de9994aa2c84039b86
sprocatitommaso <tommaso.sprocati@mail.polimi.it>
2022-04-03 17:06:02 +0200

    little fixes

--- src/test/java/it/polimi/ingsw/controller/subcontrollers/DiningRoomControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/subcontrollers/DiningRoomControllerTest.java
@@ -36,9 +36,9 @@ class DiningRoomControllerTest {
         sg1 = new StudentGroup(Color.BLUE, 3);
         sg2 = new StudentGroup(Color.BLUE, 2);
 
-        board.addToDiningRoomOf(0, sg0);
-        board.addToDiningRoomOf(1, sg1);
-        board.addToDiningRoomOf(2, sg2);
+        board.addToDiningRoomOf(0, sg0.clone());
+        board.addToDiningRoomOf(1, sg1.clone());
+        board.addToDiningRoomOf(2, sg2.clone());
 
     }
 
