a8051e22437cce365081baf6b811c224199b4091
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-04-05 14:45:46 +0200

    Added exception package and abstract exceptions

---
+++ src/main/java/it/polimi/ingsw/exceptions/EriantysException.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.exceptions;
+
+public abstract class EriantysException extends Exception{
+
+    public EriantysException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/exceptions/EriantysRuntimeException.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.exceptions;
+
+public abstract class EriantysRuntimeException extends RuntimeException{
+
+    public EriantysRuntimeException(String message){
+        super(message);
+    }
+}
