605b01199cbdd266bde5a58f3efbda4d3ced43bd
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-04-12 19:32:42 +0200

    Finished documentation for board package

--- src/main/java/it/polimi/ingsw/model/board/Island.java
+++ src/main/java/it/polimi/ingsw/model/board/Island.java
@@ -4,38 +4,74 @@ import it.polimi.ingsw.model.enumerations.Color;
 import it.polimi.ingsw.model.helpers.StudentGroup;
 import it.polimi.ingsw.model.enumerations.TowerColor;
 
+/**
+ * Abstract class corresponding to the Island entity
+ */
 public abstract class Island {
     private final StudentGroup students = new StudentGroup();
     private TowerColor teamColor;
     private boolean motherNature;
 
+    /**
+     * Adds specified students to this <code>Island</code>
+     * @param toAdd the <code>StudentGroup</code> to add
+     */
     protected void addStudents(StudentGroup toAdd) {
         toAdd.transferAllTo(students);
     }
 
+    /**
+     * Returns the color of the last team that conquered this <code>Island</code>
+     * @return <code>TowerColor</code> of the team that conquered this <code>Island</code>
+     */
     public TowerColor getTeamColor() {
         return teamColor;
     }
 
+    /**
+     * Sets the color of the team that conquered this <code>Island</code>
+     * @param teamColor the color of the team
+     */
     private void setTeamColor(TowerColor teamColor) {
         this.teamColor = teamColor;
     }
 
+    /**
+     * Returns whether Mother Nature is on this <code>Island</code>
+     * @return <code>true</code> if Mother Nature is on this <code>Island</code>
+     */
     public boolean isMotherNatureOnIsland() {
         return motherNature;
     }
 
+    /**
+     * Sets whether Mother Nature is on this <code>Island</code>
+     * @param motherNature the flag specifying if Mother Nature is on this <code>Island</code>
+     */
     protected void setMotherNatureTo(boolean motherNature) {
         this.motherNature = motherNature;
     }
 
+    /**
+     * Returns the amount of students on this <code>Island</code> of the specified <code>Color</code>
+     * @param c the <code>Color</code> of the students
+     * @return Amount of students of speficied <code>Color</code>
+     */
     public int getNumStudentsByColor(Color c) {
         return students.getByColor(c);
     }
 
+    /**
+     * Conquers this <code>Island</code> with the specified team color
+     * @param teamColor the team conquering this <code>Island</code>
+     */
     protected void conquerIsland(TowerColor teamColor) {
         setTeamColor(teamColor);
     }
 
+    /**
+     * Returns the amount of Islands (>1 if MultiIsland)
+     * @return Amount of islands
+     */
     public abstract int getNumIslands();
 }
--- src/main/java/it/polimi/ingsw/model/board/ProfessorTracker.java
+++ src/main/java/it/polimi/ingsw/model/board/ProfessorTracker.java
@@ -2,9 +2,15 @@ package it.polimi.ingsw.model.board;
 
 import it.polimi.ingsw.model.enumerations.Color;
 
+/**
+ * Class used to track the owners of the various professors.
+ */
 public class ProfessorTracker implements Cloneable {
     private final int[] owners;
 
+    /**
+     * Sole constructor, sets each <code>Color</code> to <code>null</code> owner (ID -1)
+     */
     public ProfessorTracker() {
         owners = new int[Color.NUM_COLORS];
 
@@ -13,10 +19,20 @@ public class ProfessorTracker implements Cloneable {
         }
     }
 
+    /**
+     * Returns the ID of the owner of the specified <code>Color</code>
+     * @param c the <code>Color</code> to check
+     * @return ID of the owner of the professor
+     */
     public int getOwnerIDByColor(Color c) {
         return owners[c.ordinal()];
     }
 
+    /**
+     * Sets new owner ID for specified professor <code>Color</code>
+     * @param playerID the ID of the new owner
+     * @param c the <code>Color</code> of the professor
+     */
     protected void setOwnerIDByColor(int playerID, Color c) {
         owners[c.ordinal()] = playerID;
     }
