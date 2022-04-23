0359b2fddfefe4ebbb250474e11a2d537a19f28e
sprocatitommaso <tommaso.sprocati@mail.polimi.it>
2022-04-23 12:19:51 +0200

    completed CloudStateController testing with 100% coverage

--- src/test/java/it/polimi/ingsw/controller/round/CloudStateControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/round/CloudStateControllerTest.java
@@ -2,11 +2,16 @@ package it.polimi.ingsw.controller.round;
 
 import it.polimi.ingsw.controller.subcontrollers.DiningRoomController;
 import it.polimi.ingsw.controller.subcontrollers.IslandController;
+import it.polimi.ingsw.exceptions.board.CloudUnavailableException;
 import it.polimi.ingsw.exceptions.game.IllegalActionException;
+import it.polimi.ingsw.exceptions.game.NullCharacterCardException;
+import it.polimi.ingsw.exceptions.game.NullPlayerException;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.MatchInfo;
+import it.polimi.ingsw.model.board.Board;
 import it.polimi.ingsw.model.enumerations.*;
 import it.polimi.ingsw.model.helpers.Parameters;
+import it.polimi.ingsw.model.helpers.StudentGroup;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,7 +44,7 @@ public class CloudStateControllerTest {
 
     @AfterEach
     void tearDown() {
-        Game.getInstance();
+        Game.resetInstance();
         MatchInfo.resetInstance();
         game = null;
         matchInfo = null;
@@ -60,7 +65,51 @@ public class CloudStateControllerTest {
 
     @Test
     void definePlayOrderTest() {
+        matchInfo.removePlayer();
+        matchInfo.removePlayer();
         game.playCard(0, Card.CARD_2);
         game.playCard(1, Card.CARD_1);
+        game.playCard(2, Card.CARD_3);
+
+        controller.definePlayOrder();
+
+        assertEquals(1, matchInfo.getPlayOrder().peek());
+        matchInfo.removePlayer();
+        assertEquals(2, matchInfo.getPlayOrder().peek());
+        matchInfo.removePlayer();
+        assertEquals(0, matchInfo.getPlayOrder().peek());
+    }
+
+    @Test
+    void collectFromCloudTest_nullPlayer() {
+        matchInfo.removePlayer();
+        matchInfo.removePlayer();
+
+        assertThrowsExactly(NullPlayerException.class, () -> controller.collectFromCloud(0));
+    }
+
+    @Test
+    void collectFromCloudTest_cloudUnavailable() {
+        game.createClouds(1);
+        assertThrowsExactly(CloudUnavailableException.class, () -> controller.collectFromCloud(0));
+    }
+
+    @Test
+    void collectFromCloudTest() {
+        game.createClouds(1);
+        game.refillClouds(2);
+        Board board = game.getBoard();
+        StudentGroup cloudStudents = board.getClouds().get(0).getStudents();
+        board.addSchool(game.getPlayerByID(0));
+
+        try {
+            controller.collectFromCloud(0);
+        } catch (CloudUnavailableException e) {
+            throw new RuntimeException(e);
+        }
+
+        for(Color c : Color.values()) {
+            assertEquals(cloudStudents.getByColor(c), board.getSchoolByPlayerID(0).getNumStudentsInEntranceByColor(c));
+        }
     }
 }
