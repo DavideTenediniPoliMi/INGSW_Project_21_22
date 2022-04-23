fdc35f112d682eb5dd14cdb7348199bfe435b9b9
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-04-23 16:14:29 +0200

    Complete testing

--- src/test/java/it/polimi/ingsw/controller/round/StudentStateControllerExpertTest.java
+++ src/test/java/it/polimi/ingsw/controller/round/StudentStateControllerExpertTest.java
@@ -3,14 +3,13 @@ package it.polimi.ingsw.controller.round;
 import it.polimi.ingsw.controller.subcontrollers.DiningRoomController;
 import it.polimi.ingsw.controller.subcontrollers.IslandController;
 import it.polimi.ingsw.exceptions.game.BadParametersException;
-import it.polimi.ingsw.exceptions.game.ExpertModeDisabledException;
 import it.polimi.ingsw.exceptions.game.IllegalActionException;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.MatchInfo;
-import it.polimi.ingsw.model.enumerations.CardBack;
-import it.polimi.ingsw.model.enumerations.TowerColor;
-import it.polimi.ingsw.model.enumerations.TurnState;
+import it.polimi.ingsw.model.characters.CharacterCard;
+import it.polimi.ingsw.model.enumerations.*;
 import it.polimi.ingsw.model.helpers.Parameters;
+import it.polimi.ingsw.model.helpers.StudentGroup;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +25,11 @@ public class StudentStateControllerExpertTest {
     void setUp() {
         game = Game.getInstance();
         game.addPlayer(0, "lollo", TowerColor.BLACK, CardBack.CB_1, true);
+        game.giveCoinToPlayer(0);
+        game.getBoard().addSchool(game.getPlayerByID(0));
         game.addPlayer(1, "lello", TowerColor.WHITE, CardBack.CB_2, true);
+
+        game.instantiateCharacterCard(CharacterCards.EXCHANGE_STUDENTS.ordinal());
 
         matchInfo = MatchInfo.getInstance();
         matchInfo.setSelectedNumPlayer(2);
@@ -61,5 +64,28 @@ public class StudentStateControllerExpertTest {
     @Test
     public void testBuyCharacterCardBadParam() {
         assertThrowsExactly(BadParametersException.class, () -> controller.buyCharacterCard(3));
+    }
+
+    @Test
+    public void activateTest() {
+        Game game = Game.getInstance();
+        game.getBoard().addToEntranceOf(0, new StudentGroup(Color.BLUE, 2));
+        game.getBoard().addToEntranceOf(0, new StudentGroup(Color.RED, 2));
+        game.transferStudentToDiningRoom(0,Color.BLUE);
+        game.transferStudentToDiningRoom(0,Color.BLUE);
+
+        controller.buyCharacterCard(0);
+        Parameters params = new Parameters();
+        params.setFromOrigin(new StudentGroup(Color.RED, 2));
+        params.setFromDestination(new StudentGroup(Color.BLUE, 2));
+        params.setPlayerID(0);
+        controller.setCardParameters(params);
+
+        controller.activateCard();
+
+        assertAll(
+                () -> assertEquals(2, game.getBoard().getSchoolByPlayerID(0).getNumStudentsInDiningRoomByColor(Color.RED)),
+                () -> assertEquals(2, game.getBoard().getSchoolByPlayerID(0).getNumStudentsInEntranceByColor(Color.BLUE))
+        );
     }
 }
