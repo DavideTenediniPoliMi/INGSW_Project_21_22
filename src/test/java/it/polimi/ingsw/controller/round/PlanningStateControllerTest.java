346437b3a57336a3b0dcd8fe424c37e743c8f43c
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-04-23 12:46:43 +0200

    Full test coverage for PlanningStateController

--- src/test/java/it/polimi/ingsw/controller/round/PlanningStateControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/round/PlanningStateControllerTest.java
@@ -2,20 +2,19 @@ package it.polimi.ingsw.controller.round;
 
 import it.polimi.ingsw.controller.subcontrollers.DiningRoomController;
 import it.polimi.ingsw.controller.subcontrollers.IslandController;
+import it.polimi.ingsw.exceptions.game.BadParametersException;
 import it.polimi.ingsw.exceptions.game.IllegalActionException;
+import it.polimi.ingsw.exceptions.player.CardUsedException;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.MatchInfo;
-import it.polimi.ingsw.model.enumerations.CardBack;
-import it.polimi.ingsw.model.enumerations.Color;
-import it.polimi.ingsw.model.enumerations.TowerColor;
-import it.polimi.ingsw.model.enumerations.TurnState;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.enumerations.*;
 import it.polimi.ingsw.model.helpers.Parameters;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlanningStateControllerTest {
     PlanningStateController controller;
@@ -34,6 +33,7 @@ public class PlanningStateControllerTest {
         matchInfo.setStateType(TurnState.STUDENTS);
         matchInfo.setExpertMode(false);
         matchInfo.setNumPlayersConnected(3);
+
         matchInfo.addPlayer(0);
         matchInfo.addPlayer(1);
 
@@ -60,5 +60,72 @@ public class PlanningStateControllerTest {
                 () -> assertThrowsExactly(IllegalActionException.class, () -> controller.setCardParameters(new Parameters())),
                 () -> assertThrowsExactly(IllegalActionException.class, () -> controller.activateCard())
         );
+    }
+
+    @Test
+    public void testDefinePlayOrder() {
+        matchInfo.removePlayer();
+        matchInfo.removePlayer();
+
+        game.playCard(0, Card.CARD_1);
+        game.playCard(1, Card.CARD_5);
+        game.playCard(2, Card.CARD_3);
+
+        controller.definePlayOrder();
+
+        assertEquals(0, matchInfo.getCurrentPlayerID());
+        matchInfo.removePlayer();
+
+        assertEquals(2, matchInfo.getCurrentPlayerID());
+        matchInfo.removePlayer();
+
+        assertEquals(1, matchInfo.getCurrentPlayerID());
+        matchInfo.removePlayer();
+    }
+
+    @Test
+    public void testPlayCard() {
+        try {
+            controller.playCard(1); //Plays CARD_2
+        } catch (CardUsedException e) {
+            e.printStackTrace();
+        }
+        Player player = Game.getInstance().getPlayerByID(0);
+        assertEquals(Card.CARD_2, player.getSelectedCard());
+    }
+
+    @Test
+    public void testPlayCard_invalidID() {
+        assertThrowsExactly(BadParametersException.class, () -> controller.playCard(-1));
+        assertThrowsExactly(BadParametersException.class, () -> controller.playCard(999));
+    }
+
+    @Test
+    public void testPlayCard_alreadyUsed() {
+        try {
+            controller.playCard(0);
+        } catch (CardUsedException e) {
+            e.printStackTrace();
+        }
+        matchInfo.removePlayer();
+
+        assertThrowsExactly(CardUsedException.class, () -> controller.playCard(0));
+    }
+
+    @Test
+    public void testPlayCard_alreadyUsed_onlyAvailable() {
+        for(int i = 0; i < 9; i++){
+            game.playCard(1, Card.values()[i]);
+        }
+
+        try {
+            controller.playCard(9);
+        } catch (CardUsedException e) {
+            e.printStackTrace();
+        }
+        matchInfo.removePlayer();
+
+        assertDoesNotThrow( () -> controller.playCard(9) );
+        assertEquals(Card.CARD_10, game.getPlayerByID(1).getSelectedCard());
     }
 }
