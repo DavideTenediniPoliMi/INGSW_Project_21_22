6c96124fe4188def330d81e4db593aed47698b94
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-05-02 17:12:06 +0200

    Implemented CommandFactory

--- src/main/java/it/polimi/ingsw/network/commands/CommandFactory.java
+++ src/main/java/it/polimi/ingsw/network/commands/CommandFactory.java
@@ -1,11 +1,175 @@
 package it.polimi.ingsw.network.commands;
 
+import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.controller.LobbyController;
 import it.polimi.ingsw.exceptions.game.BadParametersException;
+import it.polimi.ingsw.model.enumerations.CardBack;
+import it.polimi.ingsw.model.enumerations.Color;
+import it.polimi.ingsw.model.enumerations.TowerColor;
+import it.polimi.ingsw.network.parameters.CardParameters;
 import it.polimi.ingsw.network.parameters.RequestParameters;
 
 public class CommandFactory {
 
-    public static Command createCommand(RequestParameters params) throws BadParametersException {
-        return null;
+    private final GameController gameController;
+    private final LobbyController lobbyController;
+
+    public CommandFactory(LobbyController lobbyController, GameController gameController) {
+        this.gameController = gameController;
+        this.lobbyController = lobbyController;
+    }
+
+    public Command createCommand(RequestParameters params) throws BadParametersException {
+        /*
+         * General nullcheck before creating a new Command
+         */
+        if(params == null) {
+            throw new BadParametersException("RequestParamers is null");
+        }else if(lobbyController == null) {
+            throw new BadParametersException("LobbyController is null");
+        }else if(gameController == null) {
+            throw new BadParametersException("GameController is null");
+        }
+
+        switch(params.getCommandType()) {
+            /*
+             * Lobby-related commands
+             */
+            case JOIN:
+                /* Params needed:
+                 *  - PlayerID
+                 *  - Name
+                 */
+                int playerID = params.getIndex();
+                String name = params.getName();
+                if(playerID >= 0 && name != null && name.length() > 0) {
+                    return new LobbyJoinCommand(playerID, name, lobbyController);
+                }
+                throw new BadParametersException("JOIN command - Given params:" +
+                        " (playerID: " + playerID + ", Name: " + name + ")");
+
+            case SEL_TOWERCOLOR:
+                /* Params needed:
+                 *  - PlayerID
+                 *  - TowerColor
+                 */
+                playerID = params.getIndex();
+                TowerColor team = params.getTowerColor();
+                if(playerID >= 0 && team != null) {
+                    return new LobbySelectTeamCommand(playerID, team, lobbyController);
+                }
+                throw new BadParametersException("SELECT TEAM command - Given params:" +
+                        " (playerID: " + playerID + ", Team: " + team + ")");
+
+            case SEL_CARDBACK:
+                /* Params needed:
+                 *  - PlayerID
+                 *  - CardBack
+                 */
+                playerID = params.getIndex();
+                CardBack cardBack = params.getCardBack();
+                if(playerID >= 0 && cardBack != null) {
+                    return new LobbySelectCBCommand(playerID, cardBack, lobbyController);
+                }
+                throw new BadParametersException("SELECT CARDBACK command - Given params:" +
+                        " (playerID: " + playerID + ", CardBack: " + cardBack + ")");
+
+            case READY_UP:
+                /* Params needed:
+                 *  - PlayerID
+                 *  - Ready
+                 */
+                playerID = params.getIndex();
+                boolean ready = params.isReady();
+                if(playerID >= 0) {
+                    return new LobbyReadyStatusCommand(playerID, ready, lobbyController);
+                }
+                throw new BadParametersException("READY STATUS command - Given params:" +
+                        " (playerID: " + playerID + ", Ready: " + ready + ")");
+
+            /*
+             * Game-related commands
+             */
+            case PLAY_CARD:
+                /* Params needed:
+                 *  - CardIndex
+                 */
+                int cardIndex = params.getIndex();
+                if(cardIndex >= 0) {
+                    return new PlayCardCommand(cardIndex, gameController);
+                }
+                throw new BadParametersException("PLAY CARD command - Given params:" +
+                        " (cardIndex: " + cardIndex +")");
+            case TRANSFER_STUDENT_TO_ISLAND:
+                /* Params needed:
+                 *  - IslandIndex
+                 *  - Color
+                 */
+                int islandIndex = params.getIndex();
+                Color color = params.getColor();
+                if(islandIndex >= 0 && color != null) {
+                    return new TransferToIslandCommand(islandIndex, color, gameController);
+                }
+                throw new BadParametersException("TRANSFER TO ISLAND command - Given params:" +
+                        " (islandIndex: " + islandIndex + ", color:" + color + ")");
+            case TRANSFER_STUDENT_TO_DINING_ROOM:
+                /* Params needed:
+                 *  - Color
+                 */
+                color = params.getColor();
+                if(color != null) {
+                    return new TransferToDiningCommand(color, gameController);
+                }
+                throw new BadParametersException("TRANSFER TO DINING command - Given params:" +
+                        " (color: null)");
+            case MOVE_MN:
+                /* Params needed:
+                 *  - DestIndex
+                 */
+                int destIndex = params.getIndex();
+                if(destIndex >= 0) {
+                    return new MoveMNCommand(destIndex, gameController);
+                }
+                throw new BadParametersException("MOVE MN command - Given params:" +
+                        " (destIndex: " + destIndex + ")");
+            case BUY_CHARACTER_CARD:
+                /* Params needed:
+                 *  - CardIndex
+                 */
+                cardIndex = params.getIndex();
+                if(cardIndex >= 0) {
+                    return new BuyCharacterCardCommand(cardIndex, gameController);
+                }
+                throw new BadParametersException("BUY CHARACTER CARD command - Given params:" +
+                        " (cardIndex: " + cardIndex + ")");
+            case SET_CARD_PARAMETERS:
+                /* Params needed:
+                 *  - CardIndex
+                 *  - CardParameters
+                 */
+                cardIndex = params.getIndex();
+                CardParameters cardParams = params.getCardParams();
+                if(cardIndex >= 0 && cardParams != null) {
+                    return new SetCardParametersCommand(cardIndex, cardParams, gameController);
+                }
+                throw new BadParametersException("SET PARAMETERS command - Given params:" +
+                        " (cardIndex: " + cardIndex + ", cardParams:" + cardParams + ")");
+            case ACTIVATE_CARD:
+                return new ActivateCharacterCardCommand(gameController);
+            case COLLECT_FROM_CLOUD:
+                /* Params needed:
+                 *  - CloudIndex
+                 */
+                int cloudIndex = params.getIndex();
+                if(cloudIndex >= 0) {
+                    return new CollectCloudCommand(cloudIndex, gameController);
+                }
+                throw new BadParametersException("COLLECT CLOUD command - Given params:" +
+                        " (cloudIndex: " + cloudIndex + ")");
+            case DISCONNECT:
+                return null;
+            default:
+                throw new BadParametersException("No such action: " + params.getCommandType());
+        }
     }
 }
