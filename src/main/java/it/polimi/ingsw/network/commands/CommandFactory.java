e2b42af845ba6824f5be4300ebdfd823d601306c
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-05-02 16:14:16 +0200

    Created CommandFactory

---
+++ src/main/java/it/polimi/ingsw/network/commands/CommandFactory.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.network.commands;
+
+import com.google.gson.JsonObject;
+import it.polimi.ingsw.exceptions.game.BadParametersException;
+
+public class CommandFactory {
+
+    public static Command createCommand(JsonObject jsonObject) throws BadParametersException {
+        return null;
+    }
+}
