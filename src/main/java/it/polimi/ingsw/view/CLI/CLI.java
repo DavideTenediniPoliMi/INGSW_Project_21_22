7c994277d8e8cd15171087fde02d551089cbdba2
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-05-10 19:45:28 +0200

    Add jansi dependency

---
+++ src/main/java/it/polimi/ingsw/view/CLI/CLI.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.view.CLI;
+
+import org.fusesource.jansi.AnsiConsole;
+
+public class CLI {
+        public static final String ANSI_CLS = "\u001b[2J";
+        public static final String ANSI_HOME = "\u001b[H";
+        public static final String ANSI_BOLD = "\u001b[1m";
+        public static final String ANSI_AT55 = "\u001b[10;10H";
+        public static final String ANSI_REVERSEON = "\u001b[7m";
+        public static final String ANSI_NORMAL = "\u001b[0m";
+        public static final String ANSI_WHITEONBLUE = "\u001b[37;44m";
+
+        public static void main(String args[]){
+            AnsiConsole.systemInstall();
+            AnsiConsole.sysOut().println(ANSI_CLS);
+            AnsiConsole.sysOut().println
+                    (ANSI_AT55 + ANSI_REVERSEON + "Hello world" + ANSI_NORMAL);
+            AnsiConsole.sysOut().println
+                    (ANSI_HOME + ANSI_WHITEONBLUE + "Hello world" + ANSI_NORMAL);
+            AnsiConsole.sysOut().print
+                    (ANSI_BOLD + "Press a key..." + ANSI_NORMAL);
+            try {System.in.read();}catch(Exception e){}
+            AnsiConsole.sysOut().println(ANSI_CLS);
+            AnsiConsole.systemInstall();
+        }
+}
