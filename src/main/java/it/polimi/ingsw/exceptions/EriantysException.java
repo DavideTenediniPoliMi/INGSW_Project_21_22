73fed710d8ea8002f2647607b1a533c92234d6af
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-05-16 08:38:52 +0200

    Fixed exception messages

--- src/main/java/it/polimi/ingsw/exceptions/EriantysException.java
+++ src/main/java/it/polimi/ingsw/exceptions/EriantysException.java
@@ -4,8 +4,13 @@ package it.polimi.ingsw.exceptions;
  * Abstract class for checked exceptions
  */
 public abstract class EriantysException extends Exception{
-
+    private final String message;
     public EriantysException(String message){
-        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/it/polimi/ingsw/exceptions/EriantysRuntimeException.java
+++ src/main/java/it/polimi/ingsw/exceptions/EriantysRuntimeException.java
@@ -4,8 +4,14 @@ package it.polimi.ingsw.exceptions;
  * Abstract class for unchecked exceptions
  */
 public abstract class EriantysRuntimeException extends RuntimeException{
+    private final String message;
 
     public EriantysRuntimeException(String message){
-        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
