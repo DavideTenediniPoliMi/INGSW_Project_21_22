300b78809300e801ac76a97267ddc3cb3e9885a4
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-05-20 00:20:11 +0200

    Add game print

--- src/main/java/it/polimi/ingsw/view/viewStates/GameViewState.java
+++ src/main/java/it/polimi/ingsw/view/viewStates/GameViewState.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.view.viewStates;
 
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.MatchInfo;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.utils.StringUtils;
+
 public class GameViewState extends ViewState {
     public GameViewState(ViewState oldViewState) {
         super(oldViewState);
@@ -7,11 +12,14 @@ public class GameViewState extends ViewState {
 
     @Override
     public String print(boolean... params) {
-        return null;
+        return StringUtils.listToString(Game.getInstance().print());
     }
 
     @Override
     public void printCLIPrompt() {
+        int activePlayerID = MatchInfo.getInstance().getCurrentPlayerID();
+        Player activePlayer = Game.getInstance().getPlayerByID(activePlayerID);
+        appendBuffer("It's " + activePlayer.getName() + "'s turn!");
     }
 
     @Override
