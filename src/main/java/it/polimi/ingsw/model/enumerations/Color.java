dab0ee6f18c1f863ce2454fcb1e342e6e1395ad7
DavideTenediniPoliMi <davide.tenedini@mail.polimi.it>
2022-06-20 21:50:24 +0200

    Game init up to players

--- src/main/java/it/polimi/ingsw/model/enumerations/Color.java
+++ src/main/java/it/polimi/ingsw/model/enumerations/Color.java
@@ -1,14 +1,32 @@
 package it.polimi.ingsw.model.enumerations;
 
+import javafx.scene.image.Image;
+
 /**
  * Class to hold the different student colors available in the game.
  */
 public enum Color {
-    BLUE,
-    GREEN,
-    PINK,
-    RED,
-    YELLOW;
+    BLUE("/images/teacher_blue.png","/images/student_blue.png"),
+    GREEN("/images/teacher_green.png","/images/student_green.png"),
+    PINK("/images/teacher_pink.png","/images/student_pink.png"),
+    RED("/images/teacher_red.png","/images/student_red.png"),
+    YELLOW("/images/teacher_yellow.png","/images/student_yellow.png");
 
     public static final int NUM_COLORS = 5;
+
+    Color(String pathProf, String pathStud) {
+        this.pathProf = pathProf;
+        this.pathStud = pathStud;
+    }
+
+    private final String pathProf;
+    private final String pathStud;
+
+    public Image getProfImage() {
+        return new Image(pathProf);
+    }
+
+    public Image getStudImage() {
+        return new Image(pathStud);
+    }
 }
--- src/main/java/it/polimi/ingsw/view/gui/controllers/GameController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/GameController.java
@@ -1,14 +1,22 @@
 package it.polimi.ingsw.view.gui.controllers;
 
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.MatchInfo;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.board.School;
+import it.polimi.ingsw.model.enumerations.Color;
+import it.polimi.ingsw.view.gui.GUI;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameController extends FXController {
-    @FXML
-    private AnchorPane player1;
     @FXML
     private AnchorPane player2;
     @FXML
@@ -232,8 +240,234 @@ public class GameController extends FXController {
     @FXML
     private AnchorPane studentBag;
 
+    private final List<Label> otherUsernames = new ArrayList<>();
+    private final List<ImageView> otherTowers = new ArrayList<>();
+    private final List<Label> otherNumTowers = new ArrayList<>();
+    private final List<Label> otherNumCoins = new ArrayList<>();
+    private final List<GridPane> otherEntrance = new ArrayList<>();
+    private final List<GridPane> otherDiningRoom = new ArrayList<>();
+    private final List<AnchorPane> clouds = new ArrayList<>();
+    private final List<GridPane> cloudStudents = new ArrayList<>();
+    private final List<ImageView> cards = new ArrayList<>();
+    private final List<GridPane> islandStudents = new ArrayList<>();
+    private final List<ImageView> islandMN = new ArrayList<>();
+    private final List<ImageView> islandTowers = new ArrayList<>();
+    private final List<ImageView> bridges = new ArrayList<>();
+    private final List<ImageView> charCards = new ArrayList<>();
+    private final List<GridPane> charCardsStudents = new ArrayList<>();
+    private final List<Label> charCardsCost = new ArrayList<>();
+    private final List<ImageView> studentsEntranceHero = new ArrayList<>();
+
     @FXML
     public void initialize() {
+        MatchInfo matchInfo = MatchInfo.getInstance();
 
+        prepArrays();
+
+        if(matchInfo.getSelectedNumPlayer() >= 3) {
+            player2.setVisible(true);
+            otherUsernames.add(usernameText2);
+            otherTowers.add(tower2);
+            otherNumTowers.add(numTowers2);
+            otherEntrance.add(entrance2);
+            otherDiningRoom.add(diningRoom2);
+            cloudPane3.setVisible(true);
+            clouds.add(cloudPane3);
+            cloudStudents.add(studentCloud3);
+
+            if(matchInfo.getSelectedNumPlayer() == 4) {
+                player3.setVisible(true);
+                otherUsernames.add(usernameText3);
+                otherTowers.add(tower3);
+                otherNumTowers.add(numTowers3);
+                otherEntrance.add(entrance3);
+                otherDiningRoom.add(diningRoom3);
+                cloudPane4.setVisible(true);
+                clouds.add(cloudPane4);
+                cloudStudents.add(studentCloud4);
+            } else if(matchInfo.getSelectedNumPlayer() == 3){
+                for(Node node : entranceHero.getChildren()) {
+                    if(GridPane.getRowIndex(node) == 4) {
+                        node.setVisible(true);
+                    }
+                }
+
+                for(Node node : towersHero.getChildren()) {
+                    if(GridPane.getRowIndex(node) == 3) {
+                        node.setVisible(false);
+                    }
+                }
+            }
+        }
+
+        if(matchInfo.isExpertMode()) {
+            //show coins pane for other players
+
+            otherNumCoins.add(numCoins1);
+
+            if(matchInfo.getSelectedNumPlayer() >= 3) {
+                otherNumCoins.add(numCoins2);
+
+                if(matchInfo.getSelectedNumPlayer() == 4) {
+                    otherNumCoins.add(numCoins3);
+                }
+            }
+
+            charCardPane1.setVisible(true);
+            charCardPane2.setVisible(true);
+            charCardPane3.setVisible(true);
+
+            charCards.add(characterCard1);
+            charCards.add(characterCard2);
+            charCards.add(characterCard3);
+
+            charCardsStudents.add(charCardStudents1);
+            charCardsStudents.add(charCardStudents2);
+            charCardsStudents.add(charCardStudents3);
+
+            charCardsCost.add(charCardCost1);
+            charCardsCost.add(charCardCost2);
+            charCardsCost.add(charCardCost3);
+
+            boardCoins.setVisible(true);
+        }
+
+        initPlayers();
+    }
+
+    private void prepArrays() {
+        otherUsernames.add(usernameText1);
+        otherTowers.add(tower1);
+        otherNumTowers.add(numTowers1);
+        otherEntrance.add(entrance1);
+        otherDiningRoom.add(diningRoom1);
+
+        clouds.add(cloudPane1);
+        clouds.add(cloudPane2);
+        cloudStudents.add(studentCloud1);
+        cloudStudents.add(studentCloud2);
+
+        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+        cards.add(card4);
+        cards.add(card5);
+        cards.add(card6);
+        cards.add(card7);
+        cards.add(card8);
+        cards.add(card9);
+        cards.add(card10);
+
+        islandStudents.add(studentsIsland1);
+        islandStudents.add(studentsIsland2);
+        islandStudents.add(studentsIsland3);
+        islandStudents.add(studentsIsland4);
+        islandStudents.add(studentsIsland5);
+        islandStudents.add(studentsIsland6);
+        islandStudents.add(studentsIsland7);
+        islandStudents.add(studentsIsland8);
+        islandStudents.add(studentsIsland9);
+        islandStudents.add(studentsIsland10);
+        islandStudents.add(studentsIsland11);
+        islandStudents.add(studentsIsland12);
+
+        islandMN.add(MNIsland1);
+        islandMN.add(MNIsland2);
+        islandMN.add(MNIsland3);
+        islandMN.add(MNIsland4);
+        islandMN.add(MNIsland5);
+        islandMN.add(MNIsland6);
+        islandMN.add(MNIsland7);
+        islandMN.add(MNIsland8);
+        islandMN.add(MNIsland9);
+        islandMN.add(MNIsland10);
+        islandMN.add(MNIsland11);
+        islandMN.add(MNIsland12);
+
+        islandTowers.add(towerIsland1);
+        islandTowers.add(towerIsland2);
+        islandTowers.add(towerIsland3);
+        islandTowers.add(towerIsland4);
+        islandTowers.add(towerIsland5);
+        islandTowers.add(towerIsland6);
+        islandTowers.add(towerIsland7);
+        islandTowers.add(towerIsland8);
+        islandTowers.add(towerIsland9);
+        islandTowers.add(towerIsland10);
+        islandTowers.add(towerIsland11);
+        islandTowers.add(towerIsland12);
+
+        bridges.add(bridge1);
+        bridges.add(bridge2);
+        bridges.add(bridge3);
+        bridges.add(bridge4);
+        bridges.add(bridge5);
+        bridges.add(bridge6);
+        bridges.add(bridge7);
+        bridges.add(bridge8);
+        bridges.add(bridge9);
+        bridges.add(bridge10);
+        bridges.add(bridge11);
+        bridges.add(bridge12);
+
+        for(Node node : entranceHero.getChildren()) {
+            studentsEntranceHero.add((ImageView)node);
+        }
+    }
+
+    private void initPlayers() {
+        Game game = Game.getInstance();
+        List<Player> players = game.getPlayers();
+        int otherPlayersIndex = 0;
+
+        for(Player player : players) {
+            School school = game.getBoard().getSchoolByPlayerID(player.getID());
+
+            if(player.getID() == GUI.getPlayerId()) {
+                usernameTextHero.setText(player.getName());
+                cardBackHero.setImage(player.getCardBack().getImage());
+
+                int entranceIndex = 0;
+
+                for(Color c : Color.values()) {
+                    int amount = school.getNumStudentsInEntranceByColor(c);
+
+                    for(int i = 0; i < amount; i++) {
+                        studentsEntranceHero.get(entranceIndex).setImage(c.getStudImage());
+                        entranceIndex++;
+                    }
+                }
+
+                for(Node tower : towersHero.getChildren()) {
+                    ImageView towerView = (ImageView) tower;
+
+                    towerView.setImage(player.getTeamColor().getImage());
+
+                    if(!player.isTowerHolder()) {
+                        tower.setVisible(false);
+                    }
+                }
+            } else {
+                otherUsernames.get(otherPlayersIndex).setText(player.getName());
+                otherTowers.get(otherPlayersIndex).setImage(player.getTeamColor().getImage());
+                otherNumTowers.get(otherPlayersIndex).setText(String.valueOf(school.getNumTowers()));
+
+                for(Color c : Color.values()) {
+                    Label numStud = (Label) getNodeFromSchool(c, otherEntrance.get(otherPlayersIndex));
+                    numStud.setText(String.valueOf(school.getNumStudentsInEntranceByColor(c)));
+                }
+
+                otherPlayersIndex++;
+            }
+        }
+    }
+
+    private Node getNodeFromSchool(Color c, GridPane grid) {
+        for(Node n : grid.getChildren()) {
+            if(GridPane.getColumnIndex(n) == c.ordinal() && n instanceof Label)
+                return n;
+        }
+
+        return null;
     }
 }
